a72502161865f7e0dc47b659708e7031dbefdebc
phatpedro21 <phatpedro21@hotmail.co.uk>
2015-11-25 13:56:13 +0000

    Unity project and basic movement added
    Player inertia based movement kind of added
    Also a basic sheet of plain colour sprites for use for testing

---
+++ K-Jam Unity Project/Assets/PlaneBehaviour.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneBehaviour : MonoBehaviour {
+
+	public Vector2 forwardForce;
+	public float tourqe;
+	float lastTourqe;
+
+
+	// Use this for initialization
+	void Start () {
+
+		forwardForce = Vector2.zero;
+		tourqe = 0.0f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKey (KeyCode.UpArrow))
+		{
+			forwardForce = this.transform.right;
+		}
+		if(Input.GetKey (KeyCode.LeftArrow))
+		{
+			tourqe = 1.0f;
+			lastTourqe = 1.0f;
+		}
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			tourqe = -1.0f;
+			lastTourqe = 1.0f;
+		}
+
+
+		this.rigidbody2D.AddForce (forwardForce);
+		this.rigidbody2D.AddTorque (tourqe);
+		if (forwardForce.x > 0) 
+		{
+			forwardForce.x -= Time.deltaTime;
+		} 
+		else
+		{
+			forwardForce.x = 0;
+		}
+		if (forwardForce.y > 0) 
+		{
+			forwardForce.y -= Time.deltaTime;
+		} 
+		else
+		{
+			forwardForce.y = 0;
+		}
+
+		if (lastTourqe == 1.0f && tourqe > 0)
+		{
+			tourqe -= Time.deltaTime;
+		} 
+		else if (lastTourqe == -1.0f && tourqe < 0)
+		{
+			tourqe += Time.deltaTime;
+		}
+		else
+		{
+			tourqe = 0.0f;
+		}
+	
+	}
+}
---
+++ K-Jam Unity Project/Assets/shadowScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shadowScript : MonoBehaviour {
+
+	public GameObject shadow;
+	float height;
+
+	// Use this for initialization
+	void Start () {
+
+		height = 0.0f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			if (height < 1.0f) 
+			{
+			height += Time.deltaTime;
+			}
+			else 
+			{
+				height = 1.0f;
+			}			
+		}
+		else
+		{
+			if(height > 0.0f)
+			{
+				height -= Time.deltaTime;
+			}
+			else
+			{
+				height = 0.0f;
+			}
+		}
+
+		this.transform.position = shadow.transform.position + new Vector3 (0.0f, height);
+		this.transform.rotation = shadow.transform.rotation;
+
+		Vector2.SqrMagnitude (shadow.rigidbody2D.velocity);
+
+
+	}
+}
