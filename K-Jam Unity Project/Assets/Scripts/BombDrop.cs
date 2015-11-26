247d9d5de84eb23da55f78c93b243f4ffa9292d3
ElysianNight <alexnunns@outlook.com>
2015-11-26 16:28:19 +0000

    Revert "Score broken atm"
    This reverts commit e1b5a106eaa172c460df359f0dff069333601eff.

--- K-Jam Unity Project/Assets/Scripts/BombDrop.cs
+++ K-Jam Unity Project/Assets/Scripts/BombDrop.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 public class BombDrop : MonoBehaviour
 {		
-	int score = 0;
-	//string scoreText = "Score: 0";
-
-	void Start()
+	void Start ()
 	{
-		//FindObjectOfType<Canvas>().GetComponentInChildren<TextMesh>().text = "Score: " + score;
+	
 	}
 	
 	void OnTriggerEnter2D(Collider2D inCollider)
@@ -16,12 +13,6 @@ public class BombDrop : MonoBehaviour
 		if (inCollider.gameObject.tag == "Bomb") 
 		{
 			Destroy(this.gameObject);
-			score += 10;
-//			GetComponentInChildren<TextMesh>().text = "Score: " + score;
-
-			//scoreText = "Score: " + score;
-
-
 		}
 	}
 
